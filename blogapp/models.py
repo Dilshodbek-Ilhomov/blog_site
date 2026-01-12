7ee49fe82ee82c21be2c356f521258e4990023b9
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-01-12 05:41:19 +0500

    update

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 from django.urls import reverse
+from django.utils import timezone
 
 
 class PublishedManager(models.Manager):
@@ -10,11 +10,10 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
-    STATUS_CHOICES = (
+    CHOICES_STATUS = (
         ('draft', 'Draft'),
-        ('published', 'Published')
+        ('publish', 'publish')
     )
-
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique_for_date="publish")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
@@ -22,10 +21,12 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default='draft')
+
 
     class Meta:
-        ordering = ('-publish',)
+        ordering = ("-publish",)
+
 
     def __str__(self):
         return self.title
@@ -33,16 +34,9 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
+
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.publish.year,
-                                                 self.publish.month,
-                                                 self.publish.day,
-                                                 self.slug])
+        return reverse("")
 
-
-posts = Post.objects.filter(status='published')
-p_posts = Post.published.all()
-
-
-
-
+post = Post.objects.all()
+p_post = Post.published.all()
