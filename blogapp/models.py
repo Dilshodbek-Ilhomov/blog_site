f0b2b732384355ea9969b2ccdfb51a9081523e5c
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-01-12 06:39:32 +0500

    update

--- blogapp/models.py
+++ blogapp/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status='publish')
 
 
 class Post(models.Model):
@@ -34,7 +34,10 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
-
     def get_absolute_url(self):
-        return reverse("")
+        return reverse('blogapp:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
